a6676d298285a86f5619ddce798342233c5f4411
Fanzaatsila <fanza.atsila.tif423@polban.ac.id>
2026-06-23 14:07:47 +0700

    push

---
+++ database/seeders/AdminSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class AdminSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        User::updateOrCreate(
+            ['email' => 'admin@optiksuci.com'],
+            [
+                'name' => 'Admin Optik Suci',
+                'password' => Hash::make('admin123'),
+            ]
+        );
+    }
+}
